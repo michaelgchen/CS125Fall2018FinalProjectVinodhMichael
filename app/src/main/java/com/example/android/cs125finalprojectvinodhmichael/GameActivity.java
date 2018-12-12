b135931438c3ea172e698a39a7d68a2bb757397a
micha <michaelguanyichen@gmail.com>
2018-12-11 21:46:29 -0600

    before trying apis

--- app/src/main/java/com/example/android/cs125finalprojectvinodhmichael/GameActivity.java
+++ app/src/main/java/com/example/android/cs125finalprojectvinodhmichael/GameActivity.java
@@ -17,7 +17,6 @@ import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.TimeUnit;
